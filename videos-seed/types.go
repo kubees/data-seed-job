abebe5e601814d1bd6e5d66e231de457051ffbe9
AhmedGrati <ahmedgratti@think-it.io>
2022-11-13 14:33:01 +0100

    feat: add videos data seed

---
+++ videos-seed/types.go
@@ -0,0 +1,9 @@
+package videos_seed
+
+type Videos struct {
+	Id          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	ImageURL    string `json:"imageurl"`
+	Url         string `json:"url"`
+}
