606034126347fea958b20c2578cc8a75aae699a6
AhmedGrati <48932084+AhmedGrati@users.noreply.github.com>
2022-11-13 14:22:22 +0100

    Feat(job): Add playlist data seed (#1)
    * feat: add playlist data seeder
    
    * feat: add playlist data seeder
    
    * fix: uncomment code

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	playlist "github.com/kubees/data-seed-job/playlist-seed"
+	"os"
+)
+
+var microservice = os.Getenv("MICROSERVICE")
+var redisHost = os.Getenv("REDIS_HOST")
+var redisPort = os.Getenv("REDIS_PORT")
+var client *redis.Client
+
+func main() {
+	r := redis.NewClient(&redis.Options{
+		Addr: redisHost + ":" + redisPort,
+		DB:   0,
+	})
+	client = r
+	ctx := context.Background()
+
+	if microservice == "playlist" {
+		playlist.SeedPlaylistsData(client, ctx)
+	}
+}
